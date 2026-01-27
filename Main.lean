5106a899c3682c0778d2fdb60cd93d4be5d83c02
pandaman64 <kointosudesuyo@infoseek.jp>
2026-01-27 22:05:10 +0900

    Web server with Verso and SQLite

--- Main.lean
+++ Main.lean
@@ -9,27 +9,27 @@ open Std.Internal.IO Async
 open Verso.Output (Html)
 open Verso.Output.Html
 
--- structure Todo where
---   id : Int64
---   title : String
---   completed : Bool
--- deriving Repr
+structure Todo where
+  id : Int64
+  title : String
+  completed : Bool
+deriving Repr
 
--- instance : SQLite.Row Todo where
---   read := do
---     return {
---       id := ← SQLite.Row.read,
---       title := ← SQLite.Row.read,
---       completed := (← SQLite.Row.read) != (0 : Int64)
---     }
+instance : SQLite.Row Todo where
+  read := do
+    return {
+      id := ← SQLite.Row.read,
+      title := ← SQLite.Row.read,
+      completed := (← SQLite.Row.read) != (0 : Int64)
+    }
 
--- def renderTodo (todo : Todo) : Html :=
---   {{
---     <div>
---       <input type="checkbox" checked=s!"{todo.completed}" />
---       <span>{{todo.title}}</span>
---     </div>
---   }}
+def renderTodo (todo : Todo) : Html :=
+  {{
+    <div>
+      <input type="checkbox" checked=s!"{todo.completed}" />
+      <span>{{todo.title}}</span>
+    </div>
+  }}
 
 -- def main : IO Unit := do
 --   let db ← SQLite.open "test.db"
@@ -71,7 +71,25 @@ open Verso.Output.Html
 
 --   db.exec "DROP TABLE todos"
 
-def runServer : IO Unit := do
+structure App where
+  db : SQLite
+
+def generateHtml (app : App) : IO Html := do
+  let db := app.db
+  let selectStmt ← db.prepare "SELECT id, title, completed FROM todos"
+  let todos ← (selectStmt.resultsAs Todo).toArray
+  return {{
+    <html>
+      <body>
+        <h1>"Todo List"</h1>
+        <ul>
+          {{ todos.map ({{<li>{{renderTodo ·}}</li>}}) }}
+        </ul>
+      </body>
+    </html>
+  }}
+
+def runServer (app : App) : IO Unit := do
   let server ← TCP.Socket.Server.mk
   server.bind (Std.Net.SocketAddressV4.mk (.ofParts 127 0 0 1) 8080)
   server.listen 128
@@ -81,9 +99,21 @@ def runServer : IO Unit := do
     return {
       code := .ok,
       contentType := "text/html",
-      body := "Hello, World!\n"
+      body := (← generateHtml app).asString
     }
   (← serverTask.toIO).block
 
+def prepareDatabase (app : App) : IO Unit := do
+  let db := app.db
+  db.exec "CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy groceries', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new phone', TRUE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new car', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new house', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new boat', TRUE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new plane', FALSE)"
+
 def main : IO Unit := do
-  runServer
+  let app : App := { db := ← SQLite.open "test.db" }
+  prepareDatabase app
+  runServer app
