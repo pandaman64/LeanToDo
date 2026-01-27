9c3cd7d446029d85d9fa0c61259af0d5082b174e
pandaman64 <kointosudesuyo@infoseek.jp>
2026-01-27 19:43:33 +0900

    Add leansqlite

--- Main.lean
+++ Main.lean
@@ -1,2 +1,42 @@
-def main : IO Unit :=
-  IO.println s!"Hello, world!"
+import SQLite
+
+structure Todo where
+  id : Int64
+  title : String
+  completed : Bool
+deriving Repr
+
+instance : SQLite.Row Todo where
+  read := do
+    return {
+      id := ← SQLite.Row.read,
+      title := ← SQLite.Row.read,
+      completed := (← SQLite.Row.read) != (0 : Int64)
+    }
+
+def main : IO Unit := do
+  let db ← SQLite.open "test.db"
+  db.exec "CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy groceries', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new phone', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new car', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new house', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new boat', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new plane', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new train', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new ship', FALSE)"
+  db.exec "INSERT INTO todos (title, completed) VALUES ('Buy a new bike', FALSE)"
+
+  IO.println "----- After insert -----"
+  let selectStmt ← db.prepare "SELECT * FROM todos"
+  for todo in selectStmt.resultsAs Todo do
+    IO.println s!"{todo.id} {todo.title} {todo.completed}"
+
+  db.exec "UPDATE todos SET completed = TRUE WHERE id = 4"
+
+  IO.println "----- After update -----"
+  let selectStmt ← db.prepare "SELECT * FROM todos"
+  for todo in selectStmt.resultsAs Todo do
+    IO.println s!"{todo.id} {todo.title} {todo.completed}"
+
+  db.exec "DROP TABLE todos"
